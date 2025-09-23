c33481fd8f7cbbef68642dc4aa989ab654a27786
Dev-Russo <murilorusso10@gmail.com>
2025-09-23 11:12:45 -0300

    Padronização dos Nomes de Models - Product

--- backend/models/product_model.py
+++ backend/models/product_model.py
@@ -5,7 +5,7 @@ from database import Base
 class Product(Base):
     __tablename__ = "products"
     
-    id_product = Column(Integer, primary_key = True)
+    id = Column(Integer, primary_key = True)
     name_product = Column(String(100), nullable=False, index=True)
     description_product = Column(String, nullable=False)
     price = Column(Float, nullable=False)
