e3c3784df840d89013a5920d5bf82a5cb259a23d
Dev-Russo <murilorusso10@gmail.com>
2025-09-23 11:05:50 -0300

    Rua, Cep, Bairro, Numero, Cidade, Criado, Dia do Evento, Imagem, Status - Criado dentro de Evento

--- backend/models/event_model.py
+++ backend/models/event_model.py
@@ -8,8 +8,15 @@ class Event(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(100), nullable = False, index = True)
     description = Column(Text, nullable=False, index=True)
-    location = Column(String, nullable=False)
-    date = Column(DateTime, nullable=False)
+    street = Column(String, nullable=False)
+    cep = Column(String, nullable=False)
+    neighborhood = Column(String, nullable=False)
+    number = Column(String, nullable=False)
+    city = Column(String, nullable=False)
+    created = Column(DateTime, nullable=False)
+    event_date = Column(DateTime, nullable=False)
+    image_url = Column(String, nullable=True)
+    status = Column(String, default="active", nullable=False) # activate, cancelled, completed
     
     user_id = Column(Integer, ForeignKey('users.id', ondelete="CASCADE"))
     user = relationship("User", back_populates="events")
